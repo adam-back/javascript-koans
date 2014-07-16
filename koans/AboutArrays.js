81a6a9fe7ffb8f94e6eeff9698bd5a756860d0eb
Adam Back <adb33@email.arizona.edu>
2014-07-16 13:33:13 -0700

    Understanding array length.

--- koans/AboutArrays.js
+++ koans/AboutArrays.js
@@ -29,18 +29,18 @@ describe("About Arrays", function() {
     expect(array).toEqual([1, 2, 3]);
   });
 
-  3("should understand array length", function () {
+  it("should understand array length", function () {
     var fourNumberArray = [1, 2, 3, 4];
 
-    expect(fourNumberArray.length).toBe(FILL_ME_IN);
+    expect(fourNumberArray.length).toBe(4);
     fourNumberArray.push(5, 6);
-    expect(fourNumberArray.length).toBe(FILL_ME_IN);
+    expect(fourNumberArray.length).toBe(6);
 
     var tenEmptyElementArray = new Array(10); 
-    expect(tenEmptyElementArray.length).toBe(FILL_ME_IN);
+    expect(tenEmptyElementArray.length).toBe(10);
 
     tenEmptyElementArray.length = 5;
-    expect(tenEmptyElementArray.length).toBe(FILL_ME_IN);
+    expect(tenEmptyElementArray.length).toBe(5);
   });
 
   it("should slice arrays", function () {
